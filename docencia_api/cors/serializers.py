e6c935ab44a0edf28f261d1322d8767ec5b3d3e6
axelgolin4 <axelgolin4@gmail.com>
2022-10-19 21:20:07 -0600

    Cambios corregidos

--- docencia_api/cors/serializers.py
+++ docencia_api/cors/serializers.py
@@ -6,47 +6,47 @@ from .models import Estudiante, Docente, Curso, Ciclo, Asistencia, Facultad, Cur
 class EstudianteSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Estudiante
-                field = '__all__'
+                fields = '__all__'
 
 class DocenteSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Docente
-                field = '__all__'
+                fields = '__all__'
 
 class CursoSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Curso
-                field = '__all__'
+                fields = '__all__'
 
 class CicloSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Ciclo
-                field = '__all__'
+                fields = '__all__'
 
 class EstudianteSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Estudiante
-                field = '__all__'
+                fields = '__all__'
 
 class AsistenciaSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Asistencia
-                field = '__all__'
+                fields = '__all__'
 
 class FacultadSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Facultad
-                field = '__all__'
+                fields = '__all__'
 
 class Curso_has_EstudianteSerializer(serializers.ModelSerializer):
         class Meta:
                 model = Curso_has_Estudiante
-                field = '__all__'
+                fields = '__all__'
 
 class CategoriaAsistenciaSerializer(serializers.ModelSerializer):
         class Meta:
                 model = CategoriaAsistencia
-                field = '__all__'
+                fields = '__all__'
                 
 
 
